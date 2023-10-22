8a92fd7518e95e12cc70a4f611b2d9d188750132
Vitor Hugo Migoto de Gouvêa <56165095+vitormigoto@users.noreply.github.com>
2023-10-22 14:02:38 +0000

    Exemplo de Arrays e exibição do primeiro valor

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// Arrays são estrutura de dados que permitem armazenar e manipular uma coleções de valores
+// Para se declarar uma Array usamos os colchetes [] 
+
+const minha_array = ["Valor1", "Valor2", "Valor3"]
+
+// As arrays começam suas posições sempre em 0
+// Obtemos o primeiro valor colocando o indice entre colchetes
+
+console.log(minha_array[0])
